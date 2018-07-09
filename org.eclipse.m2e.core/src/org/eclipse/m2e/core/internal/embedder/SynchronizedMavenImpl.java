00e3a06cc133d7d66679958eec0f223bd1cf1e46
helospark <helospark@gmail.com>
2018-07-09 20:33:34 +0200

    Unfortunatelly more synchronization is needed. Maybe this is not the right approach

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/embedder/SynchronizedMavenImpl.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/embedder/SynchronizedMavenImpl.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.m2e.core.internal.embedder;
 
+import java.io.File;
 import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
@@ -17,6 +18,13 @@ import org.eclipse.core.runtime.IProgressMonitor;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.ConfigurationContainer;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuildingRequest;
 
 import org.eclipse.m2e.core.embedder.IMavenConfiguration;
 
@@ -34,4 +42,38 @@ public class SynchronizedMavenImpl extends MavenImpl {
     }
   }
 
+  public MavenExecutionResult readMavenProject(File pomFile, ProjectBuildingRequest configuration)
+      throws CoreException {
+    synchronized(writeLock) {
+      return super.readMavenProject(pomFile, configuration);
+    }
+  }
+
+  public <T> T getMojoParameterValue(MavenSession session, MojoExecution mojoExecution, String parameter,
+      Class<T> asType) throws CoreException {
+    synchronized(writeLock) {
+      return super.getMojoParameterValue(session, mojoExecution, parameter, asType);
+    }
+  }
+
+  public <T> T getMojoParameterValue(String parameter, Class<T> type, MavenSession session, Plugin plugin,
+      ConfigurationContainer configuration, String goal) throws CoreException {
+    synchronized(writeLock) {
+      return super.getMojoParameterValue(parameter, type, session, plugin, configuration, goal);
+    }
+  }
+
+  public Artifact resolvePluginArtifact(Plugin plugin, List<ArtifactRepository> remoteRepositories,
+      IProgressMonitor monitor) throws CoreException {
+    synchronized(writeLock) {
+      return super.resolvePluginArtifact(plugin, remoteRepositories, monitor);
+    }
+  }
+
+  public MojoExecution setupMojoExecution(MavenSession session, MavenProject project, MojoExecution execution)
+      throws CoreException {
+    synchronized(writeLock) {
+      return super.setupMojoExecution(session, project, execution);
+    }
+  }
 }
